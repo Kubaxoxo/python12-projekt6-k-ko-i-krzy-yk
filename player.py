ff4ecb1b737d485daaf426ac0f0d415343d77b6f
Kubaxoxo <kubakruger1@gmail.com>
2022-02-15 12:12:30 +0100

    python12-projekt6-kolko i krzyzyk

---
+++ player.py
@@ -0,0 +1,30 @@
+import math
+import random
+
+
+class Player():
+    def __init__(self, letter):
+        self.letter = letter
+
+    def get_move(self, game):
+        pass
+
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\' Twoja kolej. Input rusz się o (0-9): ')
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print('Nieprawidłowy kwadrat. Spróbuj ponownie. ')
+                
+        return val
